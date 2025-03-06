2a1cefbd3dc873e2557fe8f26becde8d960f4a26
mustafamohdprogrammer <mohdmustafa969@gmail.com>
2025-03-06 16:58:33 +0530

    Updated endpoints and prepared for deployment

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-const createUser = require("./Routes/CreateUser")
-const DisplayData = require("./Routes/DisplayData")
-const OrderData = require("./Routes/OrderData")
+const createUser = require("./Routes/CreateUser");
+const DisplayData = require("./Routes/DisplayData");
+const OrderData = require("./Routes/OrderData");
 const loginRoute = require("./Routes/LoginUser");
-
 
 const connectDatabase = require('./db');
 
 connectDatabase();
 
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-type,Accept");
-    next();
-})
+// Middleware for handling CORS
+app.use((req, res, next) => {
+    const allowedOrigins = [
+        "http://localhost:3000", // Local development
+        "https://mustafasbistro.vercel.app", // Deployed frontend
+    ];
+    const origin = req.headers.origin;
 
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true"); // If using cookies or credentials
+    next();
+});
+
+// Middleware to parse JSON requests
 app.use(express.json());
 
-app.use("/api",createUser)
-app.use("/api",DisplayData)
-app.use("/api",OrderData)
+// Routes
+app.use("/api", createUser);
+app.use("/api", DisplayData);
+app.use("/api", OrderData);
 app.use("/api", loginRoute);
 
-app.get("/",(req,res) =>{
+// Test Route
+app.get("/", (req, res) => {
     res.send("Hello, World!");
-})
-app.listen(port,() =>{
+});
+
+// Start Server
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
